d97aa3b80fd076b9e6d63a7f4eab394fc4d23833
John Levon <john.levon@joyent.com>
2020-03-20 18:14:39 +0000

    TOOLS-2440 need a job-free thoth (#181)
    Reviewed by: Angela Fong <angela.fong@joyent.com>
    Reviewed by: Brian Bennett <brian.bennett@joyent.com>
    Reviewed by: Trent Mick <trentm@gmail.com>

--- Makefile
+++ Makefile
@@ -1,19 +1,65 @@
+# Copyright 2020 Joyent, Inc.
+
 UNAME=$(shell uname -s | tr "[:upper:]" "[:lower:]")
 VER=$(shell json -f package.json version)
+NAME := thoth
+RELEASE_TARBALL := $(NAME)-$(UNAME)-$(VER).tar.gz
+LATEST_TARBALL := $(NAME)-$(UNAME)-latest.tar.gz
+ROOT := $(shell pwd)
 
-node_modules: package.json
-	npm install --production
-	@touch node_modules
+NODE_PREBUILT_VERSION=v8.17.0
+NODE_PREBUILT_TAG=gz
+NODE_PREBUILT_IMAGE=5417ab20-3156-11ea-8b19-2b66f5e7a439
 
-.PHONY: release github-release
-release: thoth-$(UNAME)-$(VER).tar.gz
+#
+# Skip buildenv validation entirely as it's not happy with the lack of a build
+# image etc.
+#
+ENGBLD_SKIP_VALIDATE_BUILDENV = true
+ENGBLD_USE_BUILDIMAGE = false
+ENGBLD_REQUIRE := $(shell git submodule update --init deps/eng)
+include ./deps/eng/tools/mk/Makefile.defs
+TOP ?= $(error Unable to access eng.git submodule Makefiles.)
+RELSTAGEDIR := /tmp/$(NAME)-$(STAMP)
 
+ifeq ($(shell uname -s),SunOS)
+	include ./deps/eng/tools/mk/Makefile.node_prebuilt.defs
+else
+	NPM=npm
+	NODE=node
+	NPM_EXEC=$(shell which npm)
+endif
+
+.PHONY: all
+all: | $(NPM_EXEC)
+	$(NPM) install
+
+DISTCLEAN_FILES += ./node_modules
+
+.PHONY: release
+release: all
+	@echo "Building $(RELEASE_TARBALL)"
+	mkdir -p $(RELSTAGEDIR)/opt/custom/thoth
+	cp -r \
+		$(ROOT)/analyzers \
+		$(ROOT)/bin \
+		$(ROOT)/lib \
+		$(ROOT)/node_modules \
+		$(ROOT)/package.json \
+		$(RELSTAGEDIR)/opt/custom/thoth
+	mkdir -p $(RELSTAGEDIR)/opt/custom/thoth/build/
+	cp -r $(ROOT)/build/node $(RELSTAGEDIR)/opt/custom/thoth/build/
+	(cd $(RELSTAGEDIR) && $(TAR) -I pigz -cf $(ROOT)/$(RELEASE_TARBALL) .)
+	@rm -rf $(RELSTAGEDIR)
+
+.PHONY: publish
 publish: release
-	./tools/publish.sh "$(VER)"
+	mput -f $(ROOT)/$(RELEASE_TARBALL) /thoth/public/$(RELEASE_TARBALL)
+	mput -f $(ROOT)/$(RELEASE_TARBALL) /thoth/public/$(LATEST_TARBALL)
 
-thoth-$(UNAME)-$(VER).tar.gz:
-	./tools/build-release.sh
+include ./deps/eng/tools/mk/Makefile.deps
 
-clean:
-	rm -rf node_modules opt
-	rm -f thoth-$(UNAME)-$(VER).tar.gz
+ifeq ($(shell uname -s),SunOS)
+	include ./deps/eng/tools/mk/Makefile.node_prebuilt.targ
+endif
+include ./deps/eng/tools/mk/Makefile.targ
---
+++ lib/thoth-init.sh
@@ -0,0 +1,239 @@
+#!/bin/bash
+#
+# Copyright 2020 Joyent, Inc.
+#
+# This initializes a `thoth debug` or `thoth analyze` instance. It runs either
+# as a bash "initfile", or in the context of a Manta job.
+#
+
+#
+# If we were run non-interactively, $BASH_ENV points here: make sure a new bash
+# shell doesn't run us again.
+#
+unset BASH_ENV
+
+thoth_fatal()
+{
+	echo thoth: "$*" 1>&2
+	exit 1
+}
+
+#
+# Unlike `thoth load` alone, this updates the index, the Manta file, and the
+# local copy ./info.json.
+#
+thoth_load()
+{
+	local update=$1
+
+	if [[ -f "$update" ]]; then
+		local tmpfile=$THOTH_TMPDIR/thoth.out.$$
+
+		cat $THOTH_INFO $update | json --deep-merge >$tmpfile
+		mv $tmpfile $THOTH_INFO
+	fi
+
+	mput -qf $THOTH_INFO $THOTH_INFO_OBJECT
+	$THOTH load $THOTH_INFO
+}
+
+#
+# "sysprops" are those at the root, not under .properties
+#
+thoth_set_sys()
+{
+	local prop=$1
+	local propfile=$THOTH_TMPDIR/thoth.prop.$$
+
+	if [[ "$#" -lt 1 ]]; then
+		thoth_fatal "failed to specify property"
+	fi
+
+	if [[ "$#" -eq 2 ]]; then
+		local propval="$2"
+	else
+		local propval=`cat | sed 's/\\\\/\\\\\\\\/g' | \
+		    sed 's/"/\\\\"/g' | sed ':a;N;$!ba;s/\\n/\\\\n/g'`
+	fi
+
+	echo "{ \"$prop\": \"$propval\" }" >$propfile
+	thoth_load $propfile
+}
+
+thoth_set()
+{
+	local prop=$1
+	local propfile=$THOTH_TMPDIR/thoth.prop.$$
+
+	if [[ "$#" -lt 1 ]]; then
+		thoth_fatal "failed to specify property"
+	fi
+
+	if [[ "$#" -eq 2 ]]; then
+		local propval="$2"
+	else
+		local propval=`cat | sed 's/\\\\/\\\\\\\\/g' | \
+		    sed 's/"/\\\\"/g' | sed ':a;N;$!ba;s/\\n/\\\\n/g'`
+	fi
+
+	echo "{ \"properties\": { \"$prop\": \"$propval\" } }" >$propfile
+	thoth_load $propfile
+}
+
+#
+# "sysprops" are those at the root, not under .properties
+#
+thoth_unset_sys()
+{
+	local tmpfile=$THOTH_TMPDIR/thoth.out.$$
+
+	if [[ "$#" -lt 1 ]]; then
+		thoth_fatal "failed to specify property"
+	fi
+
+	cat $THOTH_INFO | json -e "this.$1=undefined" >$tmpfile
+	mv $tmpfile $THOTH_INFO
+	thoth_load $propfile
+}
+
+thoth_unset()
+{
+	local tmpfile=$THOTH_TMPDIR/thoth.out.$$
+
+	if [[ "$#" -lt 1 ]]; then
+		thoth_fatal "failed to specify property"
+	fi
+
+	cat $THOTH_INFO | json -e "this.properties.$1=undefined" >$tmpfile
+	mv $tmpfile $THOTH_INFO
+	thoth_load $propfile
+}
+
+thoth_ticket()
+{
+	thoth_set_sys ticket $*
+}
+
+thoth_unticket()
+{
+	thoth_unset_sys ticket
+}
+
+thoth_analyze()
+{
+	. $THOTH_ANALYZER
+}
+
+export THOTH_TMPDIR=$(pwd)
+export THOTH_DUMP=$MANTA_INPUT_FILE
+export THOTH_NAME=$(basename $(dirname $MANTA_INPUT_FILE))
+export THOTH_INFO=$THOTH_TMPDIR/info.json
+export THOTH_DIR=$(dirname $MANTA_INPUT_OBJECT)
+export THOTH_INFO_OBJECT=$THOTH_DIR/info.json
+
+#
+# As `thoth load` only updates the index, it is the info of record, but we fall
+# back to the Manta file if needed.
+#
+if [[ ! -f $THOTH_INFO ]]; then
+	$THOTH info $THOTH_NAME >$THOTH_INFO 2>/dev/null ||
+	    mget -q $THOTH_INFO_OBJECT >$THOTH_INFO 2>/dev/null
+fi
+
+export THOTH_TYPE=$(cat $THOTH_INFO | json type)
+
+export PS1="thoth@$THOTH_NAME $ "
+export DTRACE_DOF_INIT_DISABLE=1
+
+if [[ $(cat $THOTH_INFO | json cmd) == "node" ]]; then
+	FILE_STR="$(file $THOTH_DUMP)"
+	MDB_V8=mdb_v8_ia32.so
+	export MDB_V8_DIR=$THOTH_TMPDIR/mdb
+
+	function jmget() {
+		curl -sk https://us-east.manta.joyent.com/$1
+	}
+
+	if [[ $FILE_STR == *"ELF 64-bit"* ]]; then
+		MDB_V8=mdb_v8_amd64.so
+		MDB_V8_DIR=$THOTH_TMPDIR/mdb/amd64
+	fi
+
+	if [[ ! -d $MDB_V8_DIR ]]; then
+		mkdir -p $MDB_V8_DIR
+	fi
+
+	if [[ ! -f $MDB_V8_DIR/v8.so ]]; then
+		MDB_V8_LATEST=$(jmget /Joyent_Dev/public/mdb_v8/latest)
+		jmget $MDB_V8_LATEST/$MDB_V8 >$MDB_V8_DIR/v8.so
+	fi
+
+	# a rather hacky way to auto-load v8, but it works
+	function mdb() {
+		MDBRC=$THOTH_TMPDIR/.mdbrc
+		cat ~/.mdbrc >$MDBRC 2>/dev/null || true
+		echo "::set -L \"$MDB_V8_DIR\"" >>$MDBRC
+		echo "::load v8" >>$MDBRC
+		HOME=$THOTH_TMPDIR /usr/bin/mdb "$@"
+	}
+fi
+
+if [[ -n "$THOTH_ANALYZER_OBJECT" ]]; then
+	export THOTH_ANALYZER=$THOTH_TMPDIR/$THOTH_ANALYZER_NAME
+	mget -q $THOTH_ANALYZER_OBJECT >$THOTH_ANALYZER
+else
+	unset THOTH_ANALYZER
+fi
+
+# thoth analyze ...
+
+if [[ "$THOTH_RUN_ANALYZER" = "true" ]]; then
+	if [[ -n "$THOTH_ANALYZER_DCMD" ]]; then
+
+		 #
+		 # LIBPROC_INCORE_ELF=1 prevents us from loading whatever node
+		 # binary happens to be in $PATH. This is necessary when
+		 # debugging cores that do not match the bitness of the node
+		 # binary found in $PATH.
+		 #
+		export LIBPROC_INCORE_ELF=1
+
+		mdb -e "$THOTH_ANALYZER_DCMD" "$THOTH_DUMP"
+	else
+		thoth_analyze
+	fi
+	exit $?
+fi
+
+# thoth debug ...
+
+if [[ -n "$THOTH_ANALYZER" ]]; then
+	orig=$THOTH_ANALYZER.orig
+	cp $THOTH_ANALYZER $orig
+
+	echo "thoth: analyzer $THOTH_ANALYZER_NAME is in $THOTH_ANALYZER"
+	echo "thoth: run \"thoth_analyze\" to run the analyzer"
+	echo "thoth: any changes to \$THOTH_ANALYZER will be stored upon successful exit"
+
+	# make sure thoth_*() are set
+	declare -f >tmp.initfile.$$;
+
+	if bash --init-file tmp.initfile.$$ -i; then
+		if ! cmp $THOTH_ANALYZER $orig > /dev/null 2>&1; then
+			echo "thoth: storing changes to \$THOTH_ANALYZER"
+			mput -qf $THOTH_ANALYZER $THOTH_ANALYZER_OBJECT
+			echo "thoth: done"
+		fi
+	fi
+else
+	 #
+	 # LIBPROC_INCORE_ELF=1 prevents us from loading whatever node binary
+	 # happens to be in $PATH. This is necessary when debugging cores that
+	 # do not match the bitness of the node binary found in $PATH.
+	 #
+	export LIBPROC_INCORE_ELF=1
+
+	mdb "$THOTH_DUMP"
+fi
+
+exit $?
